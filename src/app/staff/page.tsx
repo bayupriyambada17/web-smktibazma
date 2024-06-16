60d18b76ec5df5c020d2bc7d6f59343dcf6a8eef
bayupriyambada17 <bayupm.dev@gmail.com>
2024-06-17 00:01:16 +0700

    Fix query loadMore Staff

--- src/app/staff/page.tsx
+++ src/app/staff/page.tsx
@@ -2,36 +2,34 @@
 import EducatorCard from '@/components/layouts/Card/EducatorCard';
 import TitleTagInformation from '@/components/layouts/organisms/TitleTagInformation';
 import { educators } from '@/constants';
-import React, { FC, useEffect, useState } from 'react'
+import { handleScroll, loadMoreData } from '@/lib/loadMore';
+import React, { FC, useCallback, useEffect, useState } from 'react'
 
 
 interface StaffProps {
 }
 
 const Staff: FC<StaffProps> = ({ }) => {
-  const [visibleEducators, setVisibleEducators] = useState<any>(educators.slice(0, 12));
+  const [visible, setVisible] = useState<any>(educators.slice(0, 12));
   const [hasMore, setHasMore] = useState<any>(educators.length > 0);
 
-  const loadMore = () => {
-    const nextIndex = visibleEducators.length;
-    const nextBatch = educators.slice(nextIndex, nextIndex + 12);
-    setVisibleEducators([...visibleEducators, ...nextBatch]);
-    setHasMore(visibleEducators.length + nextBatch.length < educators.length);
-  }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const handleScroll = () => {
-    if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
-    if (hasMore) loadMore();
-  }
+  const loadMore = useCallback(() => {
+    loadMoreData(visible, setVisible, educators, 12, setHasMore);
+  }, [visible]);
+
+  const onScroll = useCallback(() => {
+    handleScroll(hasMore, loadMore);
+  }, [hasMore, loadMore]);
+
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [visibleEducators, hasMore, handleScroll]);
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, [onScroll])
   return (
     <div className='flex flex-col mx-auto container max-w-7xl my-4'>
       <TitleTagInformation title='Educator Organization' subTitle='This page contains the names of teaching staff, you can see all staff here' />
       <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-3 mt-8'>
-        {visibleEducators.map((educator: any, index: number) => (
+        {visible?.map((educator: any, index: number) => (
           <EducatorCard educator={educator} key={index} />
         ))}
       </div>
